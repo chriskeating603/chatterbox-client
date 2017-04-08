b74d9aa7b2f55b450235e48ae5601fffa907cee0
Hack Reactor Student <student@hackreactor.com>
2017-04-08 14:41:56 -0700

    Messages sent and received

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,16 +1,13 @@
-
-
+window.user = {};
 var app = {};
 app.init = function () {
-  $(document).ready(function () {
-    $('#send .submit').unbind('submit').bind('submit');
+    //$('#send .submit').unbind('submit').bind('submit');
     $('.username').on('click', function () {
       app.handleUsernameClick();
     });
-    $('#send .submit').on('submit', function () {
+    $('.submit #send').on('submit', function (event) {
       app.handleSubmit();
     });
-  });
 };
 app.send = function (message) {
   message = message || {
@@ -34,19 +31,24 @@ app.send = function (message) {
   });
 };
 app.fetch = function () {
-  $.ajax({
+  var obj = $.ajax({
     // This is the url you should use to communicate with the parse API server.
-    // url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
+    url: 'http://parse.sfm8.hackreactor.com/chatterbox/classes/messages',
     type: 'GET',
     contentType: 'application/json',
-    // success: function (data) {
-    //   console.log('chatterbox: Message rece');
-    // },
-    // error: function (data) {
-    //   // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
-    //   console.error('chatterbox: Failed to send message', data);
-    // }
+    success: function (data) {
+      console.log('chatterbox: Message received');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
   });
+  for (var i = 0; i < obj.responseJSON.results.length; i++) {
+    $('#chats').append(obj.responseJSON.results[i]);
+  }
+
+  //console.log(results);
 };
 app.clearMessages = function () {
   $('#chats').empty();
@@ -61,8 +63,19 @@ app.renderRoom = function (room) {
   $('#roomSelect').append($(`<a href=“#”>${room}</a>`));
 };
 app.handleUsernameClick = function () {
+
   return true;
 };
 app.handleSubmit = function () {
+  // take ID message
+  // convert message to send
+  // use send function to send message
+
+  var obj = {username: 'Nichris',
+    text: $('message').val(),
+    roomname: 'lobby'};
+    
+  app.send(obj);
+            
   return true;
 };
